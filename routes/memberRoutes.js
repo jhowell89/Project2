0715390027ce2e0b3be95d917fee9871255fe4ca
cvevera <cvevera@gmail.com>
2019-12-29 14:36:06 -0500

    added Adam's member routes file

---
+++ routes/memberRoutes.js
@@ -0,0 +1,40 @@
+var db = require("../models");
+
+module.exports = function(app) {
+  app.get("/api/members", function(req, res) {
+
+    db.Member.findAll({
+      include: [db.Post]
+    }).then(function(dbMember) {
+      res.json(dbMember);
+    });
+  });
+
+  app.get("/api/members/:id", function(req, res) {
+    db.Member.findOne({
+      where: {
+        id: req.params.id
+      },
+      include: [db.Member]
+    }).then(function(dbMember) {
+      res.json(dbMember);
+    });
+  });
+
+  app.post("/api/members", function(req, res) {
+    db.Member.create(req.body).then(function(dbMember) {
+      res.json(dbMember);
+    });
+  });
+
+  app.delete("/api/members/:id", function(req, res) {
+    db.Member.destroy({
+      where: {
+        id: req.params.id
+      }
+    }).then(function(dbMember) {
+      res.json(dbMember);
+    });
+  });
+
+};
